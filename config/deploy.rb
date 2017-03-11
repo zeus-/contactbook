fb43e8713c4f45a2089ff0ed0bf6c9fe41ce11b1
Ishan <ishan@wishpond.com>
2017-03-10 16:56:55 -0800

    Change repo url

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock '3.8.0'
 
 set :application, 'contactbook'
-set :repo_url, 'git@github.com:devdatta/contactbook.git' # Edit this to match your repository
+set :repo_url, 'git@github.com:zeus-/contactbook.git' # Edit this to match your repository
 set :branch, :master
 set :deploy_to, '/home/deploy/contactbook'
 set :pty, true
